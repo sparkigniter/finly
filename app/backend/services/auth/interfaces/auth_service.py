c2ea31cff794b45fca52ec3f33cb7fd9f1f065fb
sparkigniter <11609189+sparkigniter@users.noreply.github.com>
2026-04-03 15:46:34 +0000

    chore: auto-fix python lint issues

--- app/backend/services/auth/interfaces/auth_service.py
+++ app/backend/services/auth/interfaces/auth_service.py
@@ -3,7 +3,9 @@ from app.backend.services.auth.interfaces.user import User as UserInterface
 
 
 class AuthService(Protocol):
-    def register_user(self, email: str, password: str, username: str = None) -> UserInterface:
+    def register_user(
+        self, email: str, password: str, username: str = None
+    ) -> UserInterface:
         pass
 
     # def authenticate(self, username: str, password: str) -> bool:
--- app/backend/services/broker/interfaces/holdings.py
+++ app/backend/services/broker/interfaces/holdings.py
@@ -1,13 +1,13 @@
-
 from dataclasses import dataclass
+
 
 @dataclass
 class Holdings:
     """Class to represent the user's holdings."""
+
     symbol: str
     quantity: int
     average_price: float
     last_price: float
     pnl: float
     exchange: str
-    
